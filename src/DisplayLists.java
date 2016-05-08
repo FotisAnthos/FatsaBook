fde7bdc00354fc9581e6f35701f9bf206fcb38c1
FotisAnthos <fotistou94@gmail.com>
2016-05-08 12:20:26 +0300

    +Added Display Lists

---
+++ src/DisplayLists.java
@@ -0,0 +1,38 @@
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+
+public class DisplayLists extends JFrame{
+	
+	public DisplayLists(Object anobject)
+	{
+		super(anobject.getClass().getName() + "List");
+		JPanel contentPane = new JPanel();
+		
+		JList l = new JList();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		pack();
+		this.setVisible(true);
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+	}
+	
+	
+	
+
+}
