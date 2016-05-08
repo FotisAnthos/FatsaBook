b736bae736d79b763410cb9e5e734c3ace627100
FotisAnthos <fotistou94@gmail.com>
2016-05-08 14:52:46 +0300

    Strugling with JList
    TODO Continue with the listeners

--- src/DisplayLists.java
+++ src/DisplayLists.java
@@ -1,15 +1,28 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionListener;
 
 public class DisplayLists extends JFrame{
 	
-	public DisplayLists(Object anobject)
+	public DisplayLists(ArrayList<Object> objects)
 	{
-		super(anobject.getClass().getName() + "List");
+		super(objects.getClass().getName() + "List");
 		JPanel contentPane = new JPanel();
 		
+		Object[] ob = objects.toArray(new Object[objects.size()]);//TODO check this again
+		
 		JList l = new JList();
+		contentPane.add(l);
+		l.setListData(ob);
+		l.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+		
 		
 		
 		
@@ -32,6 +45,13 @@ public class DisplayLists extends JFrame{
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 	}
 	
+	class listener implements ActionListener
+	 {
+		 public void actionPerformed(ActionEvent e)
+		 	{
+			 	//TODO 			 	
+			}
+	 }
 	
 	
 
