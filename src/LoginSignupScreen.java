d2a8532d96ef30b74f3aeb24f5e1401765fe2f2d
Panagiotis Tzafos <it14159@uom.edu.gr>
2016-05-25 01:32:25 +0300

    Some changes and fixes to the LoginSignupScreen

--- src/LoginSignupScreen.java
+++ src/LoginSignupScreen.java
@@ -9,40 +9,70 @@ public class LoginSignupScreen extends JFrame {
 	
 	private JTextField username;
 	private JPasswordField password;
-	
+	private JLabel username_label;
+	private JLabel password_label;
 	private JButton Sign_in;
 	private JButton Sign_up;
+	private JLabel welcome;
 	
 	
 	
 	public LoginSignupScreen(){
 		super("Login & Signup Screen");
 		
-		JPanel contentPane = new JPanel();		
+		JPanel contentPane = new JPanel(new BorderLayout());
 		
-		//Input Fields - username & password
+		//Username Label and input text field
+		username_label = new JLabel("Username");
 		username = new JTextField("");
-		contentPane.add(username);
-
+		
+		//Password Label and input text field
 		password = new JPasswordField(4);//4 is the minimum amount of password characters
 		password.setEchoChar('*');
-		contentPane.add(password);
+		password_label = new JLabel("Password");
 		
-		//Buttons 
+		//Buttons for Sign in and Sign up
 		Sign_in = new JButton("Sign in");
-		contentPane.add(Sign_in);
 		Sign_in.addActionListener(new SigninActionListener());
-
 		
-		
-		Sign_up = new JButton("Sign_up in");
-		contentPane.add(Sign_up);
+		Sign_up = new JButton("Sign up");
 		Sign_up.addActionListener(new SignupActionListener());
-
-
-	
+		
+		//Grouping all labels in one gridlayout panel
+		JPanel labels = new JPanel(new GridLayout(2,1));
+		labels.add(username_label);
+		labels.add(password_label);
+		
+		//Group all text fields in one gridlayout panel
+		JPanel text_fields =new JPanel(new GridLayout(2,1));
+		text_fields.add(username);
+		text_fields.add(password);
+		
+		//Group in one panel all text fields and labels
+		JPanel fields = new JPanel();
+		fields.add(labels);
+		fields.add(text_fields);
+		
+		//Group all buttons in one Panel
+		JPanel buttons = new JPanel();
+		buttons.add(Sign_in);
+		buttons.add(Sign_up);
+		
+		//Welcome Label
+		JLabel welcome = new JLabel("WELCOME TO FATSABOOK");
+		welcome.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		//Add labels, text_fields and buttons to the main panel
+		contentPane.add(welcome,BorderLayout.NORTH);
+		contentPane.add(fields,BorderLayout.CENTER);
+		contentPane.add(buttons,BorderLayout.EAST);
+		
+		//Add main panel to frame
+		this.add(contentPane);
+		
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(true);
+		this.setSize(500,200);
 	}
 
 	
@@ -62,9 +92,6 @@ public class LoginSignupScreen extends JFrame {
 				
 					
 					
-					
-				
-				
 
 		}
 	 }
