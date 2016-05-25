967c05fa5ffaa6a00a5c0ce0203c010f540a9951
SkoufisDimitris <it12136@uom.edu.gr>
2016-05-25 17:11:32 +0300

    ...

--- src/main.java
+++ src/main.java
@@ -6,9 +6,7 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		new CreateGroupScreen();
-		new CreateUserScreen();
-		//new LoginSignupScreen();
+		new LoginSignupScreen();
 		
 
 	}
