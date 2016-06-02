8a56fc6ff13f9607d38d13d90c1d4118bf71f8e3
FotisAnthos <fotistou94@gmail.com>
2016-06-02 19:50:52 +0300

    Experimenting..

--- src/Post.java
+++ src/Post.java
@@ -7,7 +7,8 @@ public class Post implements Comparable<Post> {
 	private Date date;
 	private String postText;
 	private User user;//the creator
-	
+	private User anotherUser;//points to the user timeline the post is displayed, null if displayed in a group
+	private Group agroup;//points to the group timeline the post is displayed, null if displayed in a User's timeline
 	private ArrayList<Integer> replies; //replies -> String(?) -> postID
 	private ArrayList<User> Likes; //likes -> String -> user.mail
 	private static int No_ofPosts;
@@ -16,13 +17,13 @@ public class Post implements Comparable<Post> {
 	 */
 	private User owner;
 	private int post_id;
-
 	
-
-	public Post(String postText, User creator) {
+	
+	public Post(String postText, User creator, User anotherUser) {//Used for creating posts in User's timelines
 		Timestamp ts = new Timestamp(System.currentTimeMillis());
 		Date date = new Date(ts.getTime());
 		this.date = date;
+		this.anotherUser = anotherUser;
 		
 		this.postText = postText;
 		this.user = creator;
@@ -32,8 +33,22 @@ public class Post implements Comparable<Post> {
 	}
 	
 
-	public Post(Date date, String postText, User user, ArrayList<Integer> replies, ArrayList<String> likes, User owner,
-			int post_id) {
+	public Post(String postText, User creator, Group agroup) {//Used for creating posts in Groups
+		Timestamp ts = new Timestamp(System.currentTimeMillis());
+		Date date = new Date(ts.getTime());
+		this.date = date;
+		this.agroup = agroup;
+		
+		this.postText = postText;
+		this.user = creator;
+		this.replies = null;
+		this.Likes = null;
+		No_ofPosts++;
+	}
+	
+
+	public Post(Date date, String postText, User user, ArrayList<Integer> replies, ArrayList<User> likes, User owner,
+			int post_id) {//Used when retrieving from save files
 		this.date = date;
 		this.postText = postText;
 		this.user = user;
@@ -73,7 +88,7 @@ public class Post implements Comparable<Post> {
 	*/
 	public boolean addLike(User auser)
 	{
-		if(!Likes.add(auser)) 
+		if(!Likes.add(auser))
 			{
 				System.out.printf("Like couldn't be added!!: ", auser, "\n");
 				return false;
